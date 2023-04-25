912aa9bd3ad7c5f3371702bbd09dc1065c580847
manhhung2801 <hung28012002zz@gmail.com>
2023-04-25 12:08:59 +0700

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,62 +1,18 @@
 import { useState } from 'react';
 import './App.css';
-import { Task } from './Task';
+import { Text } from './Text';
 
 
 function App() {
-  const [todoList, setTodoList] = useState([]);
+  const [showText, setShowText] = useState(false);
 
-  const[newTask, setNewTask] = useState("");
 
-  const handeChange = (event) => {
-      setNewTask(event.target.value);
-  };
-
-  const addTask = () => {
-    const task = {
-      id:  todoList.length === 0 ? 1 : todoList[todoList.length -1].id + 1,
-      taskName: newTask,
-      completed: false
-    }
-
-    setTodoList([...todoList, task]);
-  };
-
-  const deleteTask = (id) => {
-    const newTodoList = todoList.filter((task) => task.id !== id);
-
-    setTodoList(newTodoList);
-  };
-
-  const completeTask =(id) => {
-    setTodoList(
-      todoList.map((task) => {
-          if(task.id === id) {
-            return {...task, completed: true};
-          }else {
-            return task;
-          }
-      })
-    );
-  };
- 
   return <div className="App">
-        <div className="addTask">
-          <input onChange={handeChange}/>
-          <button onClick={addTask}>Add Task</button>
-        </div>
-        <div className="list">
-          {todoList.map((task) => {
-              return (<Task 
-                    taskName={task.taskName} 
-                    id={task.id} 
-                    completed={task.completed}
-                    deleteTask={deleteTask}
-                    completeTask={completeTask}
+        <button onClick={() => {
+            setShowText(!showText);
+        }}>Show Text</button>
 
-              />);
-          })}
-        </div>
+        {showText && <Text />}
   </div>
 }
 
--- src/Task.js
+++
@@ -1,9 +0,0 @@
-export const Task = (props) => {
-   return(
-        <div className="task" style={{ backgroundColor: props.completed ? "green": "white" }}>
-            <h1>{props.taskName}</h1>
-            <button onClick={() => props.completeTask(props.id)} className="b1">Complete</button>
-            <button onClick={() => props.deleteTask(props.id)} className="b2">X</button>
-        </div>
-    );
-}
